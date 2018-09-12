b3648f2815ca30d6e804818513fc65b538337671
nicomaligne <nmaligne@gmail.com>
2018-09-12 16:15:16 +0200

    add compound

--- prez/src/components/Routes.component.js
+++ prez/src/components/Routes.component.js
@@ -3,6 +3,18 @@ import { Route, Switch } from 'react-router-dom'
 import Header from './Header'
 import Step1 from './Step1'
 import items from './Shared/data'
+import { Accordion } from './Compound'
+
+const myItems = [
+	{
+		title: 'ItemA',
+		content: 'Item A content',
+	},
+	{
+		title: 'ItemB',
+		content: 'Item B content',
+	},
+]
 
 export default function Routes() {
 	return (
@@ -10,6 +22,7 @@ export default function Routes() {
 			<Header />
 			<Switch>
 				<Route path="/step1" render={() => <Step1 items={items} />} />
+				<Route path="/step2" render={() => <Accordion items={myItems} />} />
 			</Switch>
 		</React.Fragment>
 	)
